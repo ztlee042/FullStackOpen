cec0846676eb780a02affaa1029bbbf8c1e97031
ztlee042 <leeemail9898@gmail.com>
2023-02-08 15:54:04 +0100

    Part 4 - Finished 4.4

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    } else if (blogs.length === 1) {
+        return blogs[0].likes
+    } else {
+        const likes = blogs.map(d => d.likes)
+        const initialCount = 0
+        return likes.reduce((accum, current) => accum + current, initialCount)
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
