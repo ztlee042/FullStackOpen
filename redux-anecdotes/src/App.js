988eae8c0c007d58745a7a0c18abf69da813dcf0
ztlee042 <leeemail9898@gmail.com>
2023-03-21 16:25:50 +0100

    Finished 6.12

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 
 const App = () => {
   return (
     <div>
       <h1>Anecdotes</h1>
+      <Notification />
       <AnecdoteForm />
       <Filter />
       <AnecdoteList />
--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
+
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    notification: notificationReducer
   }
 })
 
