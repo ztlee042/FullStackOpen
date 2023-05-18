e1a8e859a50f943a4ec38daf1ec541a53eb944ed
ztlee042 <leeemail9898@gmail.com>
2023-05-18 16:02:35 +0200

    Prepare part8

--- part2/phonebook/src/components/Person.js
+++
@@ -1,5 +0,0 @@
-const Person = ({ persons, handleDelete }) => (
-    persons.map((person) => <div key={person.id}>{person.name} {person.number} <button onClick={() => handleDelete(person.id)}>delete</button></div>)
-)
-
-export default Person;
